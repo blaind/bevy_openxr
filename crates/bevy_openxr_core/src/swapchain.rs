5f6fe1438aca4f3b7dccc8d979bfa597c30aa6f1
Mika Vatanen <blaind@blaind.net>
2021-05-30 12:14:02 +0300

    Texture format selection

--- crates/bevy_openxr_core/src/swapchain.rs
+++ crates/bevy_openxr_core/src/swapchain.rs
@@ -1,4 +1,5 @@
 use bevy::math::{Quat, Vec3};
+use bevy::prelude::error;
 use bevy::transform::components::Transform;
 use bevy::utils::tracing::{debug, warn};
 use openxr::{Time, View};
@@ -34,7 +35,6 @@ pub struct XRSwapchain {
 }
 
 const VIEW_COUNT: u32 = 2; // FIXME get from settings
-const COLOR_FORMAT: ash::vk::Format = ash::vk::Format::R8G8B8A8_UNORM; // FIXME change!!
 
 impl XRSwapchain {
     pub fn new(device: Arc<wgpu::Device>, openxr_struct: &mut OpenXRStruct) -> Self {
@@ -68,16 +68,45 @@ impl XRSwapchain {
             .map(|f| ash::vk::Format::from_raw(*f as i32))
             .collect::<Vec<_>>();
 
-        println!(
-            "OpenXR supported swapchain formats: {:#?}",
-            vk_swapchain_formats
-        );
+        let vk_wgpu_formats = vk_swapchain_formats
+            .iter()
+            .map(|&vk_format| {
+                let hal_format: Option<gfx_hal::format::Format> = map_vk_format(vk_format);
+                let wgpu_format = match hal_format {
+                    Some(hal_format) => map_texture_format(hal_format),
+                    None => None,
+                };
 
-        let format = wgpu::TextureFormat::Rgba8Unorm;
+                (vk_format, hal_format, wgpu_format)
+            })
+            .collect::<Vec<_>>();
+
+        println!("OpenXR supported swapchain formats:");
+        for (idx, (vk, hal, wgpu)) in vk_wgpu_formats.iter().enumerate() {
+            println!(
+                "   idx={}, vk={:?} gfx_hal={:?} wgpu={:?}",
+                idx, vk, hal, wgpu
+            );
+        }
+
+        let format = vk_wgpu_formats
+            .iter()
+            .enumerate()
+            .find(|(_, (_, hal, wgpu))| hal.is_some() && wgpu.is_some())
+            .map(|(idx, (vk, hal, wgpu))| (idx, vk, hal.unwrap(), wgpu.unwrap()));
+
+        let (format_idx, vk_format, _hal_format, format) = match format {
+            Some(f) => f,
+            None => {
+                panic!(
+                    "OpenXR did not have any supported swapchain formats available. Can not continue"
+                );
+            }
+        };
 
         println!(
-            "Selected (currently hardcoded) swapchain format: {:?}",
-            COLOR_FORMAT
+            "Selected swapchain format: idx={} vk={:?} wgpu={:?}",
+            format_idx, vk_format, format
         );
 
         let handle = openxr_struct
@@ -85,11 +114,8 @@ impl XRSwapchain {
             .session
             .create_swapchain(&openxr::SwapchainCreateInfo {
                 create_flags: openxr::SwapchainCreateFlags::EMPTY,
-                usage_flags: openxr::SwapchainUsageFlags::COLOR_ATTACHMENT
-                 ,/*   | openxr::SwapchainUsageFlags::DEPTH_STENCIL_ATTACHMENT, // FIXME depth?
-                | openxr::SwapchainUsageFlags::SAMPLED, | openxr::SwapchainUsageFlags::TRANSFER_SRC
-                | openxr::SwapchainUsageFlags::TRANSFER_DST,*/
-                format: COLOR_FORMAT.as_raw() as _,
+                usage_flags: openxr::SwapchainUsageFlags::COLOR_ATTACHMENT,
+                format: vk_format.as_raw() as _,
                 sample_count: 1,
                 width: resolution.width,
                 height: resolution.height,
@@ -385,4 +411,158 @@ struct Framebuffer {
     #[allow(dead_code)]
     texture: wgpu::Texture,
     texture_view: Option<wgpu::TextureView>,
+}
+
+// TODO: this is based on gfx_backend_vulkan/conv.rs, can it be used directly?
+pub fn map_vk_format(vk_format: ash::vk::Format) -> Option<gfx_hal::format::Format> {
+    if (vk_format.as_raw() as usize) < gfx_hal::format::NUM_FORMATS
+        && vk_format != ash::vk::Format::UNDEFINED
+    {
+        Some(unsafe { std::mem::transmute(vk_format) })
+    } else {
+        None
+    }
+}
+
+// TODO: this is just a reverse map based on wgpu/wgpu-core/src/conv.rs: map_texture_format (from wgpu to hal)
+// maybe pull request to wgpu to abstract away?
+pub(crate) fn map_texture_format(
+    hal_format: gfx_hal::format::Format,
+) -> Option<wgpu::TextureFormat> {
+    use gfx_hal::format::Format as H;
+    use wgpu::TextureFormat as Tf;
+    Some(match hal_format {
+        // Normal 8 bit formats
+        H::R8Unorm => Tf::R8Unorm,
+        H::R8Snorm => Tf::R8Snorm,
+        H::R8Uint => Tf::R8Uint,
+        H::R8Sint => Tf::R8Sint,
+
+        // Normal 16 bit formats
+        H::R16Uint => Tf::R16Uint,
+        H::R16Sint => Tf::R16Sint,
+        H::R16Sfloat => Tf::R16Float,
+        H::Rg8Unorm => Tf::Rg8Unorm,
+        H::Rg8Snorm => Tf::Rg8Snorm,
+        H::Rg8Uint => Tf::Rg8Uint,
+        H::Rg8Sint => Tf::Rg8Sint,
+
+        // Normal 32 bit formats
+        H::R32Uint => Tf::R32Uint,
+        H::R32Sint => Tf::R32Sint,
+        H::R32Sfloat => Tf::R32Float,
+        H::Rg16Uint => Tf::Rg16Uint,
+        H::Rg16Sint => Tf::Rg16Sint,
+        H::Rg16Sfloat => Tf::Rg16Float,
+        H::Rgba8Unorm => Tf::Rgba8Unorm,
+        H::Rgba8Srgb => Tf::Rgba8UnormSrgb,
+        H::Rgba8Snorm => Tf::Rgba8Snorm,
+        H::Rgba8Uint => Tf::Rgba8Uint,
+        H::Rgba8Sint => Tf::Rgba8Sint,
+        H::Bgra8Unorm => Tf::Bgra8Unorm,
+        H::Bgra8Srgb => Tf::Bgra8UnormSrgb,
+
+        // Packed 32 bit formats
+        H::A2r10g10b10Unorm => Tf::Rgb10a2Unorm,
+        H::B10g11r11Ufloat => Tf::Rg11b10Float,
+
+        // Normal 64 bit formats
+        H::Rg32Uint => Tf::Rg32Uint,
+        H::Rg32Sint => Tf::Rg32Sint,
+        H::Rg32Sfloat => Tf::Rg32Float,
+        H::Rgba16Uint => Tf::Rgba16Uint,
+        H::Rgba16Sint => Tf::Rgba16Sint,
+        H::Rgba16Sfloat => Tf::Rgba16Float,
+
+        // Normal 128 bit formats
+        H::Rgba32Uint => Tf::Rgba32Uint,
+        H::Rgba32Sint => Tf::Rgba32Sint,
+        H::Rgba32Sfloat => Tf::Rgba32Float,
+
+        // Depth and stencil formats
+        H::D32Sfloat => Tf::Depth32Float,
+
+        // FIXME: check that these are really allright
+        H::X8D24Unorm => Tf::Depth24Plus,
+        //H::D32Sfloat => Tf::Depth24Plus, // double, above also
+        H::D24UnormS8Uint => Tf::Depth24Plus,
+        H::D32SfloatS8Uint => Tf::Depth24Plus,
+        /* original wgpu->hal conversion
+        Tf::Depth24Plus => {
+            if private_features.texture_d24 {
+                H::X8D24Unorm
+            } else {
+                H::D32Sfloat
+            }
+        }
+        Tf::Depth24PlusStencil8 => {
+            if private_features.texture_d24_s8 {
+                H::D24UnormS8Uint
+            } else {
+                H::D32SfloatS8Uint
+            }
+        }
+         */
+        // BCn compressed formats
+        H::Bc1RgbaUnorm => Tf::Bc1RgbaUnorm,
+        H::Bc1RgbaSrgb => Tf::Bc1RgbaUnormSrgb,
+        H::Bc2Unorm => Tf::Bc2RgbaUnorm,
+        H::Bc2Srgb => Tf::Bc2RgbaUnormSrgb,
+        H::Bc3Unorm => Tf::Bc3RgbaUnorm,
+        H::Bc3Srgb => Tf::Bc3RgbaUnormSrgb,
+        H::Bc4Unorm => Tf::Bc4RUnorm,
+        H::Bc4Snorm => Tf::Bc4RSnorm,
+        H::Bc5Unorm => Tf::Bc5RgUnorm,
+        H::Bc5Snorm => Tf::Bc5RgSnorm,
+        H::Bc6hSfloat => Tf::Bc6hRgbSfloat,
+        H::Bc6hUfloat => Tf::Bc6hRgbUfloat,
+        H::Bc7Unorm => Tf::Bc7RgbaUnorm,
+        H::Bc7Srgb => Tf::Bc7RgbaUnormSrgb,
+
+        // ETC compressed formats
+        H::Etc2R8g8b8Unorm => Tf::Etc2RgbUnorm,
+        H::Etc2R8g8b8Srgb => Tf::Etc2RgbUnormSrgb,
+        H::Etc2R8g8b8a1Unorm => Tf::Etc2RgbA1Unorm,
+        H::Etc2R8g8b8a1Srgb => Tf::Etc2RgbA1UnormSrgb,
+        H::Etc2R8g8b8a8Unorm => Tf::Etc2RgbA8Unorm,
+        //H::Etc2R8g8b8a8Unorm => Tf::Etc2RgbA8UnormSrgb, FIXME ok?
+        H::EacR11Unorm => Tf::EacRUnorm,
+        H::EacR11Snorm => Tf::EacRSnorm,
+        H::EacR11g11Unorm => Tf::EtcRgUnorm,
+        H::EacR11g11Snorm => Tf::EtcRgSnorm,
+
+        // ASTC compressed formats
+        //H::Astc4x4Srgb => Tf::Astc4x4RgbaUnorm, FIXME ok?
+        H::Astc4x4Srgb => Tf::Astc4x4RgbaUnormSrgb,
+        H::Astc5x4Unorm => Tf::Astc5x4RgbaUnorm,
+        H::Astc5x4Srgb => Tf::Astc5x4RgbaUnormSrgb,
+        H::Astc5x5Unorm => Tf::Astc5x5RgbaUnorm,
+        H::Astc5x5Srgb => Tf::Astc5x5RgbaUnormSrgb,
+        H::Astc6x5Unorm => Tf::Astc6x5RgbaUnorm,
+        H::Astc6x5Srgb => Tf::Astc6x5RgbaUnormSrgb,
+        H::Astc6x6Unorm => Tf::Astc6x6RgbaUnorm,
+        H::Astc6x6Srgb => Tf::Astc6x6RgbaUnormSrgb,
+        H::Astc8x5Unorm => Tf::Astc8x5RgbaUnorm,
+        H::Astc8x5Srgb => Tf::Astc8x5RgbaUnormSrgb,
+        H::Astc8x6Unorm => Tf::Astc8x6RgbaUnorm,
+        H::Astc8x6Srgb => Tf::Astc8x6RgbaUnormSrgb,
+        H::Astc10x5Unorm => Tf::Astc10x5RgbaUnorm,
+        H::Astc10x5Srgb => Tf::Astc10x5RgbaUnormSrgb,
+        H::Astc10x6Unorm => Tf::Astc10x6RgbaUnorm,
+        H::Astc10x6Srgb => Tf::Astc10x6RgbaUnormSrgb,
+        H::Astc8x8Unorm => Tf::Astc8x8RgbaUnorm,
+        H::Astc8x8Srgb => Tf::Astc8x8RgbaUnormSrgb,
+        H::Astc10x8Unorm => Tf::Astc10x8RgbaUnorm,
+        H::Astc10x8Srgb => Tf::Astc10x8RgbaUnormSrgb,
+        H::Astc10x10Unorm => Tf::Astc10x10RgbaUnorm,
+        H::Astc10x10Srgb => Tf::Astc10x10RgbaUnormSrgb,
+        H::Astc12x10Unorm => Tf::Astc12x10RgbaUnorm,
+        H::Astc12x10Srgb => Tf::Astc12x10RgbaUnormSrgb,
+        H::Astc12x12Unorm => Tf::Astc12x12RgbaUnorm,
+        H::Astc12x12Srgb => Tf::Astc12x12RgbaUnormSrgb,
+        _ => {
+            error!("Could not map hal format {:?} to wgpu format", hal_format);
+            return None;
+        }
+    })
 }
